6a9690b0501416d13fa2df63f99a402555863b3b
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-09-03 10:47:27 +0300

    Create Best-Selling Books (SQL for Beginners #5).py

---
+++ Best-Selling Books (SQL for Beginners #5).py
@@ -0,0 +1,19 @@
+'
+You work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your store. Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+
+books table schema
+
+name
+author
+copies_sold
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
+
+
+https://www.codewars.com/kata/591127cbe8b9fb05bd00004b/train/sql
+'
+
+SELECT name,author,copies_sold
+FROM books
+where copies_sold >5
+order by copies_sold desc
+limit 5
