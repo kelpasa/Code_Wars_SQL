8087d068f984fa4a3771c02cef4bfcb7e9f8916f
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-09-03 15:53:41 +0300

    Create Easy SQL: Counting and Grouping.sql

---
+++ Easy SQL: Counting and Grouping.sql
@@ -0,0 +1,22 @@
+'Given a demographics table in the following format:
+
+** demographics table schema **
+
+id
+name
+birthday
+race
+you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+
+** output table schema **
+
+race
+count
+
+https://www.codewars.com/kata/594633020a561e329a0000a2/train/sql
+'
+
+select race, count(race) as count 
+from demographics 
+group by race 
+order by count desc
